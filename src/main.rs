5b900d34d26e4461c3df591ee5d7fc083dc217ee
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-01-16 22:37:18 +0900

    コマンドライン引数の構造体をArgsからAppに変更し、サブコマンド機能を追加

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,27 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
-#[clap(version = "1.0", author = "T4D4")]
-struct Args {
-    arg1: String,
-    arg2: String,
+#[clap(version = "1.0")]
+struct App {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Subcommand)]
+enum Command {
+    // 新しい口座を作る
+    New,
+    // 口座に入金する
+    Deposit,
+    // 口座から出金する
+    Withdraw,
+    // CSVからインポートする
+    Import,
+    // レポートを出力する
+    Report,
 }
 
 fn main() {
     // 構造体Argsで定義した形の引数を受け取ることを機体してparseする
-    let _args = Args::parse();
+    let _args = App::parse();
 }
