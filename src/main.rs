671282dc6a5c8d2b0a0d6c9b393eeacc3d3fceb2
T4D4 <98306330+T4D4-IU@users.noreply.github.com>
2025-01-16 22:40:26 +0900

    サブコマンドのenmuでの列挙時にコメントを//から///にすることでhelpオプション時に各コマンドの説明文を表示するように更新

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,18 @@ struct App {
 
 #[derive(Subcommand)]
 enum Command {
-    // 新しい口座を作る
+    /// 新しい口座を作る
     New,
-    // 口座に入金する
+    /// 口座に入金する
     Deposit,
-    // 口座から出金する
+    /// 口座から出金する
     Withdraw,
-    // CSVからインポートする
+    /// CSVからインポートする
     Import,
-    // レポートを出力する
+    /// レポートを出力する
     Report,
 }
 
 fn main() {
-    // 構造体Argsで定義した形の引数を受け取ることを機体してparseする
     let _args = App::parse();
 }
